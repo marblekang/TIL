38a50884379687a3b09ee7ea595a99ffcd079745
marblekang <kangbm930@gmail.com>
2022-04-25 14:25:58 +0900

    feat: 조건문 추가

--- javascript/js_coding_test/programmers/level1/20220424_Reverse_integers_to_make_an_array.js
+++ javascript/js_coding_test/programmers/level1/20220424_Reverse_integers_to_make_an_array.js
@@ -16,10 +16,14 @@ n	return
 */
 
 function solution(num) {
-      let arr = num.toString().split(''); /* 문자로 변환 한후 split 함수 사용해서 한글자씩 배열에 담는다. */
-      let reverse = []; /* 데이터 하나씩 뒤집은 후  담을 배열 */
+    if(num > 10000000000){
+      return null;
+    }else{
+       let arr = num.toString().split(''); 
+      let reverse = [];
       for (let i = arr.length - 1; i >= 0; i--) { // index값 맞추기위해 .length-1 , for 문 돌때마다 i는 1씩 작아짐(i가 0 이상일때까지) 
          reverse.push(arr[i]*1); // 데이터 하나씩 배열에 추가해줌. 문자데이터를 숫자로 바꾸기 위해 *1
         }
         return reverse; //반환
+    } 
 }
